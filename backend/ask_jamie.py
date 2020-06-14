92ef216f8e48127811a44a4ca9e01ec7d8ecbcc3
Damien Cheng <damiencheng@icloud.com>
2020-06-14 19:13:37 +0800

    Fixed AskJamie response. Previously was appending all possible answers as one answer and sending to client. Also fixed scenario where AskJamie would return no possible answers.

--- backend/ask_jamie.py
+++ backend/ask_jamie.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     arg_parser.add_argument("--test_questions", type=str, help="Path to newline separated questions stored in txt file")
     arg_parser.add_argument("--output", type=str, help="Path to json output file")
     args = arg_parser.parse_args()
-    
+
     # with open(args.test_questions, 'r', encoding='utf-8') as f:
     #     questions = [line.strip() for line in f]
     question = args.test_questions
-        
+
     url = "https://va.ecitizen.gov.sg/flexAnsWS/ifaqservice.asmx/AskWSLanguage"
     partial_payload = {"ProjectId": 7536554, "RecordQuestion": "yes", "SessionId": 0, "TextLanguage": "en"}
     results = defaultdict(list)
@@ -32,12 +32,15 @@ if __name__ == '__main__':
 
     # parse html string
     raw_answers = [e.find("{urn:agathos-com:agathos-solutions:flexanswer:data:v1}Text").text for e in root.find("Responses").findall("Response")]
-    
-    for raw_answer in raw_answers:
+
+    if len(raw_answers) == 0:
+        print("I am sorry, your question does not provide enough detail for me to answer. Please rephrase your question.")
+    else:
+        raw_answer = raw_answers[0]
         answer = BeautifulSoup(raw_answer, features='html.parser').text
         print(answer)
         results[question].append(answer)
-            
+
     # assert len(results) == len(questions)
     # with open(args.output, 'w', encoding='utf-8') as f:
     #     json.dump(results, f)
